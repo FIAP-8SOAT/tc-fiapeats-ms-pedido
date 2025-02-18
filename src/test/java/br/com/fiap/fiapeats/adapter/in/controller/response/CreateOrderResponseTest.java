975510d0f1794170a9be7427ad61fb3108744425
Monica Thamiris Silva Ferreira <monica.tsf@gmail.com>
2025-02-18 14:25:17 -0300

    feat: adicionando mais testes unitarios1

--- src/test/java/br/com/fiap/fiapeats/adapter/in/controller/response/CreateOrderResponseTest.java
+++ src/test/java/br/com/fiap/fiapeats/adapter/in/controller/response/CreateOrderResponseTest.java
@@ -18,7 +18,61 @@ public class CreateOrderResponseTest {
         LocalDateTime createTimestamp = LocalDateTime.now();
         String qrCode = "QR12345";
 
-        CreateOrderResponse response = CreateOrderResponse.builder()
+        CreateOrderResponse response = CreateOrderResponse.builder().build();
+        response.setOrderId(orderId);
+        response.setStatus(status);
+        response.setTaxId(taxId);
+        response.setTimeWaiting(timeWaiting);
+        response.setCreateTimestamp(createTimestamp);
+        response.setQrCode(qrCode);
+
+        assertEquals(orderId, response.getOrderId());
+        assertEquals(taxId, response.getTaxId());
+        assertEquals(status, response.getStatus());
+        assertEquals(timeWaiting, response.getTimeWaiting());
+        assertEquals(createTimestamp, response.getCreateTimestamp());
+        assertEquals(qrCode, response.getQrCode());
+    }
+
+    @Test
+    void testToString() {
+        String orderId = "12345";
+        String taxId = "12345678901";
+        String status = "PENDING";
+        int timeWaiting = 10;
+        LocalDateTime createTimestamp = LocalDateTime.of(2025,10,13,15,10);
+        String qrCode = "QR12345";
+
+        CreateOrderResponse response = CreateOrderResponse.builder().build();
+        response.setOrderId(orderId);
+        response.setStatus(status);
+        response.setTaxId(taxId);
+        response.setTimeWaiting(timeWaiting);
+        response.setCreateTimestamp(createTimestamp);
+        response.setQrCode(qrCode);
+
+        String expectedString = "CreateOrderResponse(" +
+                "orderId=" + orderId +
+                ", taxId=12345678901" +
+                ", status=PENDING" +
+                ", timeWaiting=10" +
+                ", createTimestamp=2025-10-13T15:10" +
+                ", qrCode=QR12345" +
+                ')';
+
+        assertEquals(expectedString, response.toString());
+    }
+
+    @Test
+    void testEquals_sameValues_shouldReturnTrue() {
+        String orderId = "12345";
+        String taxId = "12345678901";
+        String status = "PENDING";
+        int timeWaiting = 10;
+        LocalDateTime createTimestamp = LocalDateTime.of(2025,10,13,15,10);
+        String qrCode = "QR12345";
+
+        CreateOrderResponse response1 = CreateOrderResponse.builder()
                 .orderId(orderId)
                 .taxId(taxId)
                 .status(status)
@@ -27,11 +81,45 @@ public class CreateOrderResponseTest {
                 .qrCode(qrCode)
                 .build();
 
-        assertEquals(orderId, response.getOrderId());
-        assertEquals(taxId, response.getTaxId());
-        assertEquals(status, response.getStatus());
-        assertEquals(timeWaiting, response.getTimeWaiting());
-        assertEquals(createTimestamp, response.getCreateTimestamp());
-        assertEquals(qrCode, response.getQrCode());
+        CreateOrderResponse response2 = CreateOrderResponse.builder()
+                .orderId(orderId)
+                .taxId(taxId)
+                .status(status)
+                .timeWaiting(timeWaiting)
+                .createTimestamp(createTimestamp)
+                .qrCode(qrCode)
+                .build();
+
+        assertEquals(response1, response2);
+    }
+
+    @Test
+    void testHashCode_sameValues_shouldReturnSameHashCode() {
+        String orderId = "12345";
+        String taxId = "12345678901";
+        String status = "PENDING";
+        int timeWaiting = 10;
+        LocalDateTime createTimestamp = LocalDateTime.of(2025,10,13,15,10);
+        String qrCode = "QR12345";
+
+        CreateOrderResponse response1 = CreateOrderResponse.builder()
+                .orderId(orderId)
+                .taxId(taxId)
+                .status(status)
+                .timeWaiting(timeWaiting)
+                .createTimestamp(createTimestamp)
+                .qrCode(qrCode)
+                .build();
+
+        CreateOrderResponse response2 = CreateOrderResponse.builder()
+                .orderId(orderId)
+                .taxId(taxId)
+                .status(status)
+                .timeWaiting(timeWaiting)
+                .createTimestamp(createTimestamp)
+                .qrCode(qrCode)
+                .build();
+
+        assertEquals(response1.hashCode(), response2.hashCode());
     }
 }
