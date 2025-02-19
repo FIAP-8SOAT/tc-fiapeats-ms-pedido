2707bf4469f34c81e9fbb5080d0f88e69b82e8b6
Monica Thamiris Silva Ferreira <monica.tsf@gmail.com>
2025-02-18 23:02:03 -0300

    feat: adicionando teste

--- src/test/java/br/com/fiap/fiapeats/adapter/in/controller/contracts/response/ErrorResponseTest.java
+++ src/test/java/br/com/fiap/fiapeats/adapter/in/controller/contracts/response/ErrorResponseTest.java
@@ -2,9 +2,23 @@ package br.com.fiap.fiapeats.adapter.in.controller.contracts.response;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ErrorResponseTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorName("Not Found")
+                .errorCode(404)
+                .message("Resource not found")
+                .build();
+
+        assertNotNull(errorResponse.getTimestamp());
+        assertEquals("Not Found", errorResponse.getErrorName());
+        assertEquals(404, errorResponse.getErrorCode());
+        assertEquals("Resource not found", errorResponse.getMessage());
+    }
 
     @Test
     void testEquals_sameValues_shouldReturnTrue() {
@@ -38,5 +52,21 @@ public class ErrorResponseTest {
                 .build();
 
         assertNotEquals(response.hashCode(), response2.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorName("Unauthorized")
+                .errorCode(401)
+                .message("Authentication required")
+                .build();
+
+        String expectedString = "ErrorResponse(timestamp=" + errorResponse.getTimestamp() +
+                ", errorName=Unauthorized, errorCode=401, message=Authentication required)";
+
+        assertTrue(errorResponse.toString().contains("Unauthorized"));
+        assertTrue(errorResponse.toString().contains("401"));
+        assertTrue(errorResponse.toString().contains("Authentication required"));
     }
 }
